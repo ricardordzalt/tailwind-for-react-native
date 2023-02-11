c19ade2b6609be69ad0ca206c148e18e990058bc
ricardordzalt <ricardo.rdzalt@gmail.com>
2023-02-11 12:05:00 -0700

    .

--- example/App.tsx
+++ example/App.tsx
@@ -22,52 +22,29 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-//  {
-//    marginTop: 32,
-//    paddingLeft: 24,
-//    paddingRigh: 24,
-//  }
 const SectionContainer = styled(View)`
-  mt-32
-  px-24
-`;
+    mt-32
+    px-24
+  `;
 
-//  {
-//    fontSize: 24,
-//    fontWeight: 600,
-//    color: '#000',
-//    Next style applies only on dark mode
-//    color: '#fff',
-//  }
 const SectionTitle = styled(Text)`
-  font-size-24
-  font-weight-600
-  color-black
-  dark:color-white
-`;
+    font-size-24
+    font-weight-600
+    color-black
+    dark:color-white
+  `;
 
-//  {
-//    marginTop: 8,
-//    fontSize: 18,
-//    fontWeight: 400,
-//    color: '#000',
-//    Next style applies only on dark mode
-//    color: '#DAE1E7', <-- Comes from provider's theme, also it comes from Colors object import
-//  }
 const SectionDescription = styled(Text)`
-  mt-8
-  font-size-18
-  font-weight-400
-  color-black
-  dark:color-light
-`;
+    mt-8
+    font-size-18
+    font-weight-400
+    color-black
+    dark:color-light
+  `;
 
-//  {
-//    fontWeight: 700,
-//  }
 const HighlightText = styled(Text)`
-  font-weight-700
-`;
+    font-weight-700
+  `;
 
 function Section({children, title}: SectionProps): JSX.Element {
   return (
@@ -80,11 +57,6 @@ function Section({children, title}: SectionProps): JSX.Element {
 
 function YourApp(): JSX.Element {
   const {tw, mode} = useTW();
-  //  {
-  //    color: '#fff',
-  //    Next style applies only on dark mode
-  //    color: '##444', <-- Comes from provider's theme, also it comes from Colors object import
-  //  }
   const backgroundStyle = tw('bg-white dark:bg-dark');
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -96,13 +68,6 @@ function YourApp(): JSX.Element {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
-        {/*
-          {
-            color: '#fff',
-            Next style applies only on dark mode
-            color: '##000', <-- Comes from provider's theme, also it comes from Colors object import
-          }
-        */}
         <View style={tw('bg-white dark:bg-black')}>
           <Section title="Step One">
             Edit <HighlightText>App.tsx</HighlightText> to change this screen
