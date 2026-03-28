794ac6d6c8dacaee1cc1b8f2d9136b5f46113158
ricardo <ricardo.rdzalt@gmail.com>
2026-03-28 00:13:06 -0700

    .

---
+++ types/index.d.ts
@@ -0,0 +1,37 @@
+import * as React from 'react';
+
+export type Mode = 'light' | 'dark' | null;
+
+export type TWRNTheme = {
+  mode?: Mode;
+  styles?: Record<string, any>;
+  colors?: Record<string, any>;
+  wpFactorConversion?: number;
+  hpFactorConversion?: number;
+};
+
+export type TWRNProviderProps = {
+  children?: React.ReactNode;
+  theme?: TWRNTheme;
+};
+
+export type TailwindStylesGeneratorType = (styles: string) => Record<string, any>;
+
+declare const useTW: () => {
+  tw: TailwindStylesGeneratorType;
+  mode: Mode;
+  toggleMode: () => void;
+  colors?: Record<string, any>;
+  hppx: (numberValue: number) => number;
+  wppx: (numberValue: number) => number;
+};
+
+declare const styled: <P extends {}>(
+  ReactComponent: React.ComponentType<P>,
+) => (
+  ...args: any[]
+) => (props: P & {style?: React.CSSProperties}) => React.ReactElement;
+
+declare const TWRNProvider: React.FC<TWRNProviderProps>;
+
+export {useTW, styled, TWRNProvider};
